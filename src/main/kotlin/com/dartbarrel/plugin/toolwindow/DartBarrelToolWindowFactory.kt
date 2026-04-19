0451c0b0528c14cf410d167124e681bee12de1c8
Ibrahim Aboelyazed <ebrahem6076@gmail.com>
2026-04-19 22:06:30 +0200

    feat(toolwindow): register Dart Barrel tool window programmatically Implement a new approach to register the Dart Barrel tool window during project startup. - Replaced the previous factory-based registration with a programmatic registration using ProjectActivity. - Ensured the tool window is only created if the project contains a pubspec.yaml file. - Updated the plugin version to 0.0.16 in gradle.properties.

--- src/main/kotlin/com/dartbarrel/plugin/toolwindow/DartBarrelToolWindowFactory.kt
+++ src/main/kotlin/com/dartbarrel/plugin/toolwindow/DartBarrelToolWindowFactory.kt
@@ -1,34 +1,53 @@
 package com.dartbarrel.plugin.toolwindow
 
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.components.service
-import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.guessProjectDir
-import com.intellij.openapi.wm.ToolWindow
-import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.openapi.util.IconLoader
+import com.intellij.openapi.wm.ToolWindowAnchor
+import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ui.content.ContentFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.function.Supplier
 
-class DartBarrelToolWindowFactory : ToolWindowFactory, DumbAware {
+private const val TOOL_WINDOW_ID = "Dart Barrel"
 
-    override fun createToolWindowContent(
-        project: Project,
-        toolWindow: ToolWindow,
-    ) {
-        val barrelToolWindow =
-            project.service<DartBarrelToolWindow>()
-        val content = ContentFactory.getInstance()
-            .createContent(
-                barrelToolWindow.getContent(),
-                "",
-                false,
-            )
-        toolWindow.contentManager.addContent(content)
-    }
+class DartBarrelToolWindowRegistrar : ProjectActivity {
 
-    override fun shouldBeAvailable(
-        project: Project,
-    ): Boolean {
-        val projectDir = project.guessProjectDir()
-        return projectDir?.findChild("pubspec.yaml") != null
+    override suspend fun execute(project: Project) {
+        val projectDir =
+            project.guessProjectDir() ?: return
+        if (projectDir.findChild("pubspec.yaml") == null) {
+            return
+        }
+
+        withContext(Dispatchers.EDT) {
+            if (project.isDisposed) return@withContext
+
+            val toolWindow = ToolWindowManager
+                .getInstance(project)
+                .registerToolWindow(TOOL_WINDOW_ID) {
+                    anchor = ToolWindowAnchor.RIGHT
+                    icon = IconLoader.getIcon(
+                        "/icons/dartBarrelToolWindow.svg",
+                        DartBarrelToolWindowRegistrar::class.java,
+                    )
+                    stripeTitle = Supplier { TOOL_WINDOW_ID }
+                    canCloseContent = false
+                }
+
+            val barrelToolWindow =
+                project.service<DartBarrelToolWindow>()
+            val content = ContentFactory.getInstance()
+                .createContent(
+                    barrelToolWindow.getContent(),
+                    "",
+                    false,
+                )
+            toolWindow.contentManager.addContent(content)
+        }
     }
 }
